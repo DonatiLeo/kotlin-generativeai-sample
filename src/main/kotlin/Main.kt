905129777e24b1cd134ab3675d6b08bb67807af9
Leo Donati <leo.donati@unice.fr>
2025-10-29 18:40:53 +0100

    Remove run code tips from Main.kt
    Removed unnecessary tips for running code.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,7 @@ package unica.ds4h.miage.donati
 
 import dev.shreyaspatil.ai.client.generativeai.GenerativeModel
 import kotlinx.coroutines.runBlocking
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() : Unit = runBlocking {
     val apiKey = System.getenv("GEMINI_API_KEY")
 
@@ -15,5 +14,4 @@ fun main() : Unit = runBlocking {
     val prompt = "raconte moi une histoire de fantômes"
     val response = monModele.generateContent(prompt)
     println(response.text)
-
 }
